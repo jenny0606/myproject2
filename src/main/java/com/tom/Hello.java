619fdc4522865001fd03882c91adea3d676b82b0
Jenny <a0981083323@gamil.com>
2022-03-04 15:01:49 +0800

    hello world for testing

---
+++ src/main/java/com/tom/Hello.java
@@ -0,0 +1,9 @@
+package com.tom;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
