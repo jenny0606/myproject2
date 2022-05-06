2206b70e0b5a59cc1334e480cbdb0f0376267649
jenny0606 <96557148+jenny0606@users.noreply.github.com>
2022-05-06 14:09:32 +0800

    jenny

---
+++ src/main/java/com/tom/sales/MyClass.java
@@ -0,0 +1,10 @@
+package com.tom.sales;
+
+public class MyClass implements Mylnterface{
+    public  void test(){
+
+    }
+    public int max(int a,int b){
+        return 1;
+    }
+}
---
+++ src/main/java/com/tom/sales/Mylnterface.java
@@ -0,0 +1,6 @@
+package com.tom.sales;
+
+public interface Mylnterface {
+    public void test();
+    public  int max(int a,int b);
+}
