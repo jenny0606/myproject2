57b426b768ef9ff3fd2b2bc66cfcc48570be0104
jenny0606 <96557148+jenny0606@users.noreply.github.com>
2022-05-20 16:30:59 +0800

    jenny

---
+++ src/main/java/com/tom/ui/MyWin.java
@@ -0,0 +1,32 @@
+package com.tom.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyWin {
+    public static void main(String[] args) {
+        JFrame frame=new JFrame();
+
+        frame.setSize(600,400);
+        frame.setLocation(300,200);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        JButton button=new JButton("ok");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("hihi");
+            }
+        });
+
+        //MyActionListener listener=new MyActionListener();
+        //button.addActionListener(listener);
+
+        frame.add(button);
+        frame.setLayout(new FlowLayout());
+        frame.setVisible(true);
+
+        System.out.println("end");
+    }
+}
