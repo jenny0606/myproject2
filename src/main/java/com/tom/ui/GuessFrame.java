20ee3e9498af64084a5b613a5384dd7b5b0b5239
jenny0606 <96557148+jenny0606@users.noreply.github.com>
2022-05-27 16:28:45 +0800

    jenny

--- src/main/java/com/tom/ui/GuessFrame.java
+++ src/main/java/com/tom/ui/GuessFrame.java
@@ -5,23 +5,42 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessFrame extends JFrame {
 
     //Fields
+    JButton button=new JButton("Guess");
+    JLabel label=new JLabel("ZZzz...");
+    JTextField number=new JTextField(8);
+    Random random=new Random();
+    int secret= random.nextInt(100)+1;
+
     //Constructors
     public GuessFrame(){
         super();
+        System.out.println("sectet:"+secret);
         setSize(600,400);
         setLocation(300,200);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        JButton button=new JButton("Hi");
-        JLabel label=new JLabel("ZZzz...");
+
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                label.setText("Hello!");
+              int num=Integer.parseInt(number.getText());
+                System.out.println(num);
+                if(num>secret){
+                    label.setText("Smaller");
+                }else if(num<secret){
+                    label.setText("Bigger");
+                }else{
+                    label.setText("Bingo");
+                }
+                //label.setText("Hello!");
             }
         });
+        add(number);
         add(button);
         add(label);
         setLayout(new FlowLayout());
@@ -32,6 +51,12 @@ public class GuessFrame extends JFrame {
 
     public static void main(String[] args) {
         GuessFrame guessFrame = new GuessFrame();
+
+
+
+
+
+
         //guessFrame.setVisible(true);
         //guessFrame.setSize(600, 400);
 
