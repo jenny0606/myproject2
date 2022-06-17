279c85ff7b73efa940d307f5d27c2b6675aa806b
jenny0606 <96557148+jenny0606@users.noreply.github.com>
2022-06-17 16:36:48 +0800

    ScoringFlie2

--- src/main/java/com/tom/score/ScoringFlie.java
+++ src/main/java/com/tom/score/ScoringFlie.java
@@ -1,17 +1,25 @@
 package com.tom.score;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class ScoringFlie {
+
+
     public static void main(String[] args) {
         try {
             FileReader fileReader = new FileReader("scores.txt");
-            int data = fileReader.read();
-            System.out.println(data);
-            data=fileReader.read();
-            System.out.println(data);
+            BufferedReader reader=new BufferedReader(fileReader);
+
+            String line=reader.readLine();
+           while (line!=null){
+               String[]token=line.split(",");
+               String  name=token[0];
+               int english=Integer.parseInt(token[1]);
+               int math=Integer.parseInt(token[2]);
+               Student stu=new Student(name,english,math);
+               stu.print();
+               line=reader.readLine();
+           }
 
         }catch(FileNotFoundException e) {
             System.out.println("檔案失敗");
@@ -19,6 +27,7 @@ public class ScoringFlie {
         }catch(IOException e){
             System.out.println("資料失敗");
         }
+        System.out.println("Testing");
     }
 
 }
